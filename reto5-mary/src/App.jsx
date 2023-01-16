d8806e92fe5e6a8df18f975318880ab3d6a1f850
zulyramm <zulyramm@gmail.com>
2023-01-15 23:11:08 -0500

    compoenente footer

--- reto5-mary/src/App.jsx
+++ reto5-mary/src/App.jsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { primaryRoute } from './routes/primaryRoute';
+import Footer from './components/sections/Footer';
 
 function App() {
   return (
-    <RouterProvider router={primaryRoute} />
+    <RouterProvider router={primaryRoute} />,
+    <Footer />
   );
 }
 
